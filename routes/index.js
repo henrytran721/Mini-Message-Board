c98086a03d443d96d5c7bd489119f7b98f6f3dee
Henry Tran <henrytran@Henry-Trans-MacBook-Pro.local>
2020-03-24 11:11:08 -0700

    added sass functionality to package.json

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 const messages = [
   {
     text: 'Hi there!',
     user: 'Henry',
-    added: new Date('2020-03-24'),
+    added: moment(new Date('2020-03-24')).format('MM/DD/YYYY'),
   },
   {
     text: 'Hello, world',
     user: 'Peter',
-    added: new Date('2020-03-24'),
+    added: moment(new Date('2020-03-24')).format('MM/DD/YYYY'),
   }
 ];
 
@@ -23,7 +24,7 @@ router.get('/new', function(req, res, next) {
 })
 
 router.post('/new', function(req, res, next) {
-  req.body.added = new Date('2020-03-24');
+  req.body.added = moment(new Date('2020-03-24')).format('MM/DD/YYYY');
   messages.push(req.body);
   console.log(messages);
   res.redirect('/');
